c21cf3c24d8e6f049a0aeb48726cd391030567ca
stephen <kejianok@126.com>
2020-01-07 10:15:16 +0800

    Initial commit

---
+++ src/components/Background.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import '../css/background.css'
+
+const Background: React.FC = () => {
+  return (
+      <div className="page-header-container">
+          <div className="container">
+              <div className="row">
+                  <div className="page-left-bg">
+                      <i className="header-annimate-element1"></i>
+                      <i className="header-annimate-element2"></i>
+                      <i className="header-annimate-element3"></i>
+                      <i className="header-annimate-element4"></i>
+                      <i className="header-annimate-element5"></i>
+                      <i className="header-annimate-element6"></i>
+                      <i className="header-annimate-element7">
+                          <div className="icon icon1">
+                              <div className="planet">
+                                  <div className="line line1"></div>
+                                  <div className="line line2"></div>
+                                  <div className="line line3"></div>
+                                  <div className="line line4"></div>
+                                  <div className="line line5"></div>
+                                  <div className="line line6"></div>
+                                  <div className="line line7"></div>
+                                  <div className="line line8"></div>
+                                  <div className="line line9"></div>
+                                  <div className="line line10"></div>
+                              </div>
+                          </div>
+                      </i>
+                      <i className="header-annimate-element8"></i>
+                      <i className="header-annimate-element9"></i>
+                      <i className="header-annimate-element10">
+                          <div className="icon icon2">
+                              <div className="planet">
+                                  <div className="line line1"></div>
+                                  <div className="line line2"></div>
+                                  <div className="line line3"></div>
+                                  <div className="line line4"></div>
+                                  <div className="line line5"></div>
+                              </div>
+                          </div>
+                      </i>
+                      <i className="header-annimate-element11"></i>
+                  </div>
+                  <div className="page-right-bg">
+                      <div className="mt-right-bg__ie8"></div>
+                      <i className="header-annimate-element12"></i>
+                      <i className="header-annimate-element13">
+                          <div className="icon icon3">
+                              <div className="planet">
+                                  <div className="line line1"></div>
+                                  <div className="line line2"></div>
+                                  <div className="line line3"></div>
+                                  <div className="line line4"></div>
+                                  <div className="line line5"></div>
+                                  <div className="line line6"></div>
+                                  <div className="line line7"></div>
+                                  <div className="line line8"></div>
+                                  <div className="line line9"></div>
+                                  <div className="line line10"></div>
+                              </div>
+                          </div>
+                      </i>
+                      <i className="header-annimate-element14"></i>
+                      <i className="header-annimate-element15"></i>
+                  </div>
+              </div>
+          </div>
+      </div>
+  );
+}
+
+export default Background;
